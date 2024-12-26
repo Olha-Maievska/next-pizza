8954ecb0c6b98234d753087f9341ef2fe4812b2a
Olha Maievska <olhamaievska91@gmail.com>
2024-12-26 14:39:01 +0100

    added error text component

--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { RequiredSymbol } from '../required-symbol'
+import { Input } from '../../ui'
 
 interface Props {
   className?: string
@@ -12,15 +14,19 @@ export const FormInput: React.FC<Props> = ({
   name,
   label,
   required,
+  ...props
 }) => {
   return (
     <div className={className}>
       {label && (
         <p>
-          {label}
-          {required && ' *'}
+          {label} {required && <RequiredSymbol />}
         </p>
       )}
+
+      <div className="relative">
+        <Input className="h-12 text-md" {...props} />
+      </div>
     </div>
   )
 }
